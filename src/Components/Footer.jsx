515802ae628a46a1740f930f2cc54673d338b697
Hizbul38 <hizbullah3870@gmail.com>
2025-11-09 23:08:11 +0600

    added the basic ui design code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer className="bg-base-200 py-10 mt-10">
+      <div className="container mx-auto text-center">
+        <h2 className="text-2xl font-bold mb-2">HomeNest</h2>
+        <p>Email: contact@homenest.com | Phone: +880 1234 567890</p>
+        <p className="mt-2 text-sm">© 2025 HomeNest. All Rights Reserved.</p>
+        <div className="flex justify-center gap-4 mt-4 text-xl">
+          <a href="#"><i className="fa-brands fa-facebook"></i></a>
+          <a href="#"><i className="fa-brands fa-twitter"></i></a>
+          <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        </div>
+      </div>
+    </footer>
         </div>
     );
 };
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import Banner from '../Components/Home/Banner';
+import PopularCities from '../Components/Home/PopularCities';
+import ChooseUs from '../Components/Home/ChooseUs';
+import Testimonials from '../Components/Home/Testimonials';
 
 const HomeLayout = () => {
     return (
@@ -10,6 +13,10 @@ const HomeLayout = () => {
             <Navbar></Navbar>
             <Banner></Banner>
             <Outlet></Outlet>
+            <PopularCities></PopularCities>
+            <ChooseUs></ChooseUs>
+            <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
